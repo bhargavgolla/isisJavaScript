40da5b994f476a9c7c3483bcf8bb701f0876a4ab
Bhargav Golla <bhargav.golla@gmail.com>
2013-08-19 00:10:04 +0530

    Addition of Angular scripts and Models from Spiro

---
+++ isisJavaScriptViewer/webapp/src/main/webapp/scripts/spiro.models.helpers.js
@@ -0,0 +1,20 @@
+﻿var Spiro;
+(function (Spiro) {
+    (function (Helpers) {
+        function getClassName(obj) {
+            var funcNameRegex = /function (.{1,})\(/;
+            var results = (funcNameRegex).exec(obj.constructor.toString());
+            return (results && results.length > 1) ? results[1] : "";
+        }
+        Helpers.getClassName = getClassName;
+
+        function typeFromUrl(url) {
+            var typeRegex = /(objects|services)\/([\w|\.]+)/;
+            var results = (typeRegex).exec(url);
+            return (results && results.length > 2) ? results[2] : "";
+        }
+        Helpers.typeFromUrl = typeFromUrl;
+    })(Spiro.Helpers || (Spiro.Helpers = {}));
+    var Helpers = Spiro.Helpers;
+})(Spiro || (Spiro = {}));
+//@ sourceMappingURL=spiro.models.helpers.js.map
---
+++ isisJavaScriptViewer/webapp/src/main/webapp/scripts/spiro.models.shims.js
@@ -0,0 +1,77 @@
+﻿var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Spiro;
+(function (Spiro) {
+    var ModelShim = (function () {
+        function ModelShim(object) {
+            this.attributes = object;
+        }
+        ModelShim.prototype.url = function () {
+            return "";
+        };
+
+        ModelShim.prototype.get = function (attributeName) {
+            return this.attributes[attributeName];
+        };
+        ModelShim.prototype.set = function (attributeName, value, options) {
+        };
+        return ModelShim;
+    })();
+    Spiro.ModelShim = ModelShim;
+
+    var HateoasModelBaseShim = (function (_super) {
+        __extends(HateoasModelBaseShim, _super);
+        function HateoasModelBaseShim(object) {
+            _super.call(this, object);
+            this.hateoasUrl = "";
+            this.method = "GET";
+            this.suffix = "";
+        }
+        HateoasModelBaseShim.prototype.url = function () {
+            return (this.hateoasUrl || _super.prototype.url.call(this)) + this.suffix;
+        };
+        HateoasModelBaseShim.prototype.onError = function (map, statusCode, warnings) {
+        };
+        HateoasModelBaseShim.prototype.preFetch = function () {
+        };
+        return HateoasModelBaseShim;
+    })(ModelShim);
+    Spiro.HateoasModelBaseShim = HateoasModelBaseShim;
+
+    var ArgumentMap = (function (_super) {
+        __extends(ArgumentMap, _super);
+        function ArgumentMap(map, parent, id) {
+            _super.call(this, map);
+            this.id = id;
+        }
+        ArgumentMap.prototype.onChange = function () {
+        };
+        ArgumentMap.prototype.onError = function (map, statusCode, warnings) {
+        };
+        return ArgumentMap;
+    })(HateoasModelBaseShim);
+    Spiro.ArgumentMap = ArgumentMap;
+
+    var CollectionShim = (function () {
+        function CollectionShim(object) {
+        }
+        CollectionShim.prototype.url = function () {
+        };
+
+        CollectionShim.prototype.add = function (models, options) {
+            this.models = this.models || [];
+
+            for (var i = 0; i < models.length; i++) {
+                var m = new this.model(models[i]);
+                this.models.push(m);
+            }
+        };
+        return CollectionShim;
+    })();
+    Spiro.CollectionShim = CollectionShim;
+})(Spiro || (Spiro = {}));
+//@ sourceMappingURL=spiro.models.shims.js.map
